123f15989a8bc9b0932bb0625eaa085db3605f59
Misaka10049 <87744356+Misaka10049@users.noreply.github.com>
2023-04-19 23:22:05 +0800

    Upload /login/

---
+++ login/users.js
@@ -0,0 +1,7 @@
+var users=[],password=[],base64_password=[]
+users.push("myh")
+password.push("86536110a0409693a349b1356abf56ba")//gotoschool
+base64_password.push("af2eb9df89dbb5be78b10f4c3ed62fe9")
+users.push("ljx")
+password.push("5c3661bef3435ef3594f0532ba67eea1")//myfriend
+base64_password.push("2e72a770fe4f8677e22a6f74dc51f679")
