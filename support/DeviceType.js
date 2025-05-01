ddff066ba0c7fa05c893eac421842b1373982e31
Misaka10049 <87744356+Misaka10049@users.noreply.github.com>
2025-05-01 15:54:17 +0800

    Delete support/DeviceType.js

--- support/DeviceType.js
+++
@@ -1,4 +0,0 @@
-var uA=navigator.userAgent;
-var isAndroid=(uA.indexOf('Android')!=-1)||(uA.indexOf('Linux')!=-1);
-var isIOS=!!uA.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-var isWechat=(uA.match(/MicroMessenger/i)=='micromessenger');
